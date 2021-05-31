ee646013e756bef0b749e5913a02d59b95a26068
adrien nhem <adrien.nhem@gmail.com>
2021-05-31 18:42:51 +0100

    display spaces

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
   def index
+    @spaces = []
+    orgid = "-MA1LVuAa2r7iEH23aFf"    
+    token = "YTZRcjdDMHhldmcxZWx0ejZmMXhjN3NOVnRuMjotTUE1Znlpb0NoZGVFRFZWMDRVUi0tTUE1ZnlpcER3eURKNkU2ajNyXw=="
+
+    url = "https://api-beta.gitbook.com/v1/owners/#{orgid}/spaces"
+    encoded_url = URI.encode(url)
+
+    response = HTTParty.get(encoded_url,  headers: {
+    Authorization: "Bearer #{token}"
+    })
+
+    parsed_response = JSON.parse(response.body)
+
+    parsed_response['items'].each do |space|
+      @spaces.push(space['baseName'])
+    end
   end
 end
