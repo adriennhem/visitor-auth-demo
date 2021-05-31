ff69c439919317feeb39eaa0d9ad090c3f4d2f4a
adrien nhem <adrien.nhem@gmail.com>
2021-05-31 19:52:12 +0100

    update design

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,11 +1,12 @@
 class AuthController < ApplicationController
     def encode_token
         # add some logic here to auth users
+        space = params[:space]
         if current_user.authorized?
             payload = { data: 'test' }
             gitbook_sign_key = '2262c4b8-2c24-4e08-bc76-3e132b0a4e8c'
             token = JWT.encode payload, gitbook_sign_key, 'HS256'
-            redirect_to "https://gitbook-demo-2.gitbook.io/project-1/?jwt_token=#{token}"
+            redirect_to "https://gitbook-demo-2.gitbook.io/#{space}/?jwt_token=#{token}"
         else
             redirect_to root_path, notice: "You are not authorized to access this content!"
         end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery");
+require('popper.js');
+require("bootstrap")
+import "../src/style.scss";
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
