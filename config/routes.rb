91428b7e3df7893f47086cd0686efc1c42dd0a1c
adrien nhem <adrien.nhem@gmail.com>
2021-05-31 17:57:31 +0100

    hide notice if no notice

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
-  get '/auth/encode_token', to: 'auth#encode_token'
+  get  '/auth/encode_token', to: 'auth#encode_token'
 end
