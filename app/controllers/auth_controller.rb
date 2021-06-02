5a361774e4e61b46b2fd2f7264655a128843ada3
adrien nhem <adrien.nhem@gmail.com>
2021-06-02 13:16:04 +0100

    update design

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,10 +1,15 @@
 class AuthController < ApplicationController
     def encode_token
-        # add some logic here to auth users
-        space = params[:space]
         if current_user.authorized?
+            space = params[:space]
             payload = { data: 'test' }
-            gitbook_sign_key = '2262c4b8-2c24-4e08-bc76-3e132b0a4e8c'
+
+            if space == "api-reference"
+                gitbook_sign_key = "781cc582-1005-44c9-9b49-17d14470e439"
+            elsif space == "docs"
+                gitbook_sign_key = "01cab9b5-52ed-402d-8c0c-e44714828873"
+            end
+
             token = JWT.encode payload, gitbook_sign_key, 'HS256'
             redirect_to "https://gitbook-demo-2.gitbook.io/#{space}/?jwt_token=#{token}"
         else
